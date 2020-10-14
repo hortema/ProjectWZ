8361dcea403a3a96e1f398271b41ba5a8a98e83a
hortema <49540656+hortema@users.noreply.github.com>
2020-10-13 21:11:46 -0400

    Working background
    This code just imports the background picture and display the background in the window.  Only part of the background is shown because we made the window smaller than the background on purpose!
    
    Will work on movement next.

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame as pg
+import sys
+
+from sys import path
+from sys import exit
+import os
+
+from pygame.locals import QUIT
+
+# setup path information for python to find files by name
+my_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(my_path)
+path.append(my_path)
+
+# preset screen size is 800 x 600
+screenx = 800
+screeny = 600
+
+# start pygame, open a screen with preset size.
+pg.init()
+screen = pg.display.set_mode((screenx,screeny)) #set your window size, (x,y)
+pg.display.set_caption("Let's get scrolling!!!")
+
+clock = pg.time.Clock()
+
+#load background image and show on screen
+bg = pg.image.load("GiantBackground.png")
+
+bgx = 0
+bgy = 0
+
+while True:
+    screen.blit(bg, (bgx,bgy))
+    pg.display.flip()
