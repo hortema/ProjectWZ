136a4579182ff672ded9bdba55a86deeb59271ef
hortema <49540656+hortema@users.noreply.github.com>
2020-11-09 20:13:10 -0500

    added continuos movement and added variables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import expy as ex
 
 from sys import path
 from sys import exit
@@ -18,8 +19,11 @@ screeny = 600
 
 # preset colours
 black = (0,0,0)
+white = (255, 255, 255) 
+green = (0, 255, 0) 
+blue = (0, 0, 128) 
 
-# start pygame, open a screen with preset size.
+# start pg, open a screen with preset size.
 pg.init()
 screen = pg.display.set_mode((screenx,screeny)) #set your window size, (x,y)
 pg.display.set_caption("Let's get scrolling!!!")
@@ -27,32 +31,45 @@ pg.display.set_caption("Let's get scrolling!!!")
 clock = pg.time.Clock()
 
 #load background image and show on screen
-bg = pg.image.load("GiantBackground.png")
+bg = pg.image.load("bgbgbg.png")
+player = pg.image.load("Player.png")
+
+fps = 60
 
 bgx = 0
 bgy = 0
 
+#def update():
+ 
+
 while True:
     QUIT = False
+
+    #inputs
+    mx,my = pg.mouse.get_pos()
+    L,M,R = pg.mouse.get_pressed() #1/0 : 1-pressed
+    keys = pg.key.get_pressed()
+
+    #update()
+    
+    if keys[pg.K_d] ==True:
+        bgx -= 5
+    if keys[pg.K_a] ==True:
+        bgx += 5
+    if keys[pg.K_s] ==True:
+        bgy -= 5
+    if keys[pg.K_w] ==True:
+        bgy += 5
 
     for event in pg.event.get():
         if event.type == pg.locals.QUIT:
             QUIT = True
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_RIGHT:
-                bgx -= 50
-            if event.key == pg.K_LEFT:
-                bgx += 50
-            if event.key == pg.K_DOWN:
-                bgy -= 50
-            if event.key == pg.K_UP:
-                bgy += 50
- 
+            
     screen.fill(black)
     screen.blit(bg, (bgx,bgy))
     pg.display.flip()
-    clock.tick(30)
-    
+    clock.tick(fps)
+
     if QUIT == True:
         pg.quit()
         break
